96527d69f052c86221be9c1eb303072396570855
l21212364 <147667250+l21212364@users.noreply.github.com>
2024-05-22 00:08:14 -0700

    Initial commit

---
+++ SistemaVentas/Reutilizable/EnumModelo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVentas.Reutilizable
+{
+    public class EnumModelo
+    {
+        public enum Modelo
+        {
+            Producto = 0,
+            Tienda = 1,
+            Proveedor = 2,
+            ProductoTienda = 3
+        }
+        
+    }
+}
